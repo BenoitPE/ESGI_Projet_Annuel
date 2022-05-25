88d99ac5b9db822782a814f6275cbb01bac0ace5
BenoitPE <benoitpegaz@gmail.com>
2022-05-25 11:21:36 +0200

    MovieAPI: Ajout d'images de basse et haute qualités

--- MovieAPI/MovieAPI/Models/Movie.cs
+++ MovieAPI/MovieAPI/Models/Movie.cs
@@ -12,19 +12,28 @@ namespace MovieAPI.Models
 
         [JsonPropertyName("adult")]
         public bool? Adult { get; set; }
-        
+
         [JsonPropertyName("poster_path")]
-        public string? Image { get; set; }
-        
+        private string? Image { get; set; }
+
+        public string? ImageLowQuality
+        {
+            get { return "https://image.tmdb.org/t/p/w500" + Image; }
+        }
+        public string? ImageHighQuality
+        {
+            get { return "https://image.tmdb.org/t/p/original" + Image; }
+        }
+
         [JsonPropertyName("release_date")]
         public DateTime? ReleaseDate { get; set; }
-        
+
         [JsonPropertyName("vote_average")]
         public float? Vote { get; set; }
-        
+
         [JsonPropertyName("genres")]
         public List<Genre>? Genres { get; set; }
-        
+
         [JsonPropertyName("credits")]
         public Credits? Credits { get; set; }
     }
