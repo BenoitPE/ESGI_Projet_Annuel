b53f270e7d9a0bff04aa198a0570accf0aa79d04
tdac2806 <tristandacosta@gmail.com>
2022-07-23 16:46:03 +0200

    UserApi : test swagger

--- UserApi/src/main/java/com/projetannuel/userapi/OpenAPIConfig.java
+++ UserApi/src/main/java/com/projetannuel/userapi/OpenAPIConfig.java
@@ -1,17 +1,26 @@
 package com.projetannuel.userapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
+@Configuration
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         License license = new License().name("Apache 2.0").url("https://springdoc.org/");
-        Info info = new Info().title("J2EE TD Spring REST API").version("v0.0.1").license(license);
-        return new OpenAPI().info(info);
+        Info info = new Info().title("User REST API").version("v1").license(license);
+        List<Server> servers = new ArrayList<Server>();
+        servers.add(new Server().url("https://userapi.youges.fr").description("Production server"))
+        return new OpenAPI().info(info).servers(servers);
     }
     @Bean
     public GroupedOpenApi completeApi() {
